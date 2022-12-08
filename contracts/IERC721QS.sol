9a4b26759996e5f2ed00574952704dd4819db738
5660.eth <76733013+5660-eth@users.noreply.github.com>
2022-12-08 18:53:13 +0800

    Update IERC721QS.sol

--- contracts/IERC721QS.sol
+++ contracts/IERC721QS.sol
@@ -1,15 +1,16 @@
  // SPDX-License-Identifier: CC0-1.0
  
- pragma solidity ^0.8.7;
+ pragma solidity ^0.8.0;
 
-interface IERC721QS {
+ interface IERC721QS {
 
     /// Logged when the guard of an NFT is changed 
     /// @notice Emitted when  the `guard` is changed
     /// The zero address for guard indicates that there is no guard address
-    event updateGuardLog(uint256 tokenId,address newGuard,address oldGuard);
+    event UpdateGuardLog(uint256 indexed tokenId,address indexed newGuard,address oldGuard);
     
-    /// @notice  Owner sets guard or guard modifies guard
+    /// @notice  Owner can set guard of the NFT and guard can modifiy guard of the NFT
+    /// If the NFT has a guard role, the owner of the NFT cannot modify guard
     /// @dev The newGuard can not be zero address
     /// Throws if `tokenId` is not valid NFT
     /// @param tokenId The NFT to get the guard address for
@@ -17,24 +18,25 @@ interface IERC721QS {
     function changeGuard(uint256 tokenId, address newGuard) external;
 
   
-    /// @notice Remove the guard of an NFT
-    /// @dev The guard address is set to 0 address
+    /// @notice Remove the guard of the NFT
     /// Only guard can remove its own guard role
-    /// Throws  if `tokenId` is not valid NFT
+    /// @dev The guard address is set to 0 address
+    /// Throws if `tokenId` is not valid NFT
     /// @param tokenId The NFT to remove the guard address for
     function removeGuard(uint256 tokenId) external;
     
     /// @notice Transfer the NFT and remove its guard role
-    /// Throws  if `tokenId` is not valid NFT
-    /// @param  from  The address of the previous owner of the NFT
-    /// @param  to  The address of NFT recipient 
-    /// @param  tokenId The NFT to get transferred for
+    /// @dev The NFT is transferred to `to`and the guard address is set to 0 address
+    /// Throws if `tokenId` is not valid NFT
+    /// @param from The address of the previous owner of the NFT
+    /// @param to The address of NFT recipient 
+    /// @param tokenId The NFT to get transferred for
     function transferAndRemove(address from,address to,uint256 tokenId) external;
 
-    /// @notice Get the guard address of an NFT
+    /// @notice Get the guard address of the NFT
     /// @dev The zero address indicates that there is no guard
     /// Throws if `tokenId` is not valid NFT
     /// @param tokenId The NFT to get the guard address for
-    /// @return The guard address for this NFT
+    /// @return The guard address for the NFT
    function guardOf(uint256 tokenId) external view returns (address);   
 }
